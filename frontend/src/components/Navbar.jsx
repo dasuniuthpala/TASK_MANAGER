ad3d2844688ded8533e57697a9b99c274b46ca5d
dasuniuthpala <168283147+dasuniuthpala@users.noreply.github.com>
2025-07-15 23:18:59 +0530

    last

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = ({ user = {}, onLogout, children }) => {
             </div>
             {/* BRAND NAME */}
             <span className='text-2xl font-extrabold bg-gradient-to-r from-fuchsia-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent tracking-wide'>
-              TaskFlow
+              TaskNest
             </span>
           </div>
           {/* RIGHT SIDE */}
